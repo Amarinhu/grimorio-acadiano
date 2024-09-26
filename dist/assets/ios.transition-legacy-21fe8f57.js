cd7bbf8637ea87af376f22607a7ea4fd9d986137
Amaral <amaral.passosgabriel@gmail.com>
2024-09-26 16:52:01 -0300

    build android

--- dist/assets/ios.transition-legacy-21fe8f57.js
+++ dist/assets/ios.transition-legacy-21fe8f57.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-88c9a495.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
+System.register(["./index-legacy-bb547612.js"],(function(e,t){"use strict";var o,n;return{setters:[e=>{o=e.l,n=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
